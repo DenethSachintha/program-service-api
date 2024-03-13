692b695c81fa1af500f35381edf7a56822230925
Sachin <kbdsjayathilaka@gmail.com>
2024-03-13 20:12:42 +0530

    Response

--- src/main/java/com/sachin/lms/programserviceapi/service/ProgramService.java
+++ src/main/java/com/sachin/lms/programserviceapi/service/ProgramService.java
@@ -1,7 +1,11 @@
 package com.sachin.lms.programserviceapi.service;
-
 import com.sachin.lms.programserviceapi.dto.request.RequestProgramDto;
+import com.sachin.lms.programserviceapi.dto.response.ResponseProgramDto;
+
+import java.util.List;
 
 public interface ProgramService {
     public void createProgram(RequestProgramDto programDto);
+
+    List<ResponseProgramDto> findAllPrograms();
 }
