0b6189edbcc382fca58876f67ddca66b1f1f3148
Sachin <kbdsjayathilaka@gmail.com>
2024-03-15 23:09:58 +0530

    Response

--- src/main/java/com/sachin/lms/programserviceapi/ProgramServiceApiApplication.java
+++ src/main/java/com/sachin/lms/programserviceapi/ProgramServiceApiApplication.java
@@ -2,10 +2,8 @@ package com.sachin.lms.programserviceapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ProgramServiceApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProgramServiceApiApplication.class, args);
