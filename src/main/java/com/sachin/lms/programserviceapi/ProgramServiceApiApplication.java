49175daf68cec9d32968f8b289ac5733689fb628
Sachin <kbdsjayathilaka@gmail.com>
2024-03-27 15:22:39 +0530

    Enable Discovery Client

--- src/main/java/com/sachin/lms/programserviceapi/ProgramServiceApiApplication.java
+++ src/main/java/com/sachin/lms/programserviceapi/ProgramServiceApiApplication.java
@@ -2,8 +2,10 @@ package com.sachin.lms.programserviceapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProgramServiceApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProgramServiceApiApplication.class, args);
